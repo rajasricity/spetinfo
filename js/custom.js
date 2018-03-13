ad94e8516c528381e18d6e496595bd889f787156
Raja Sekhar Sana <sanarajasekhar@gmail.com>
2018-03-14 01:08:32 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,116 @@ var server;
 server = "http://spetinfo.com/spetinfo/";
 $(function(){
 connect();
+if(localStorage.getItem("Phone") != null){
+	$("#lbarinner").load(server+"leftBarLog.php");
+	$("#bottom").show();
+}else{
+	$("#lbarinner").load(server+"leftBar.php");
+}
+
+
+$("#login").on('submit', function(e){
+	e.preventDefault();
+	var fdata = $("#login").serialize();
+	$.ajax({
+		url:server+"app_checkLogin.php",
+		data:fdata,
+		type:"post",
+		beforeSend: function(){
+			$("#loader").show();
+		},
+		success: function(data){
+			$("#loader").hide();
+			$("#emsg").hide();
+			if(data.Status=="Success"){
+				console.log(data);
+				localStorage.setItem("Sno",data.Sno);
+				localStorage.setItem("Phone",data.Phone);
+				location.href='index.html';
+			}else{
+				$("#emsg").show();
+				$("#emsg").html(data.Status);
+			}
+		}
+	});
+});
+
+$("#register").on('submit', function(e){
+	e.preventDefault();
+	var fdata = $("#register").serialize();
+	$.ajax({
+		url:server+"app_Register.php",
+		data:fdata,
+		type:"post",
+		beforeSend: function(){
+			$("#loader").show();
+		},
+		success: function(data){
+			$("#loader").hide();
+			$("#emsg").hide();
+			if(data.Status=="Success"){
+				console.log(data);
+				localStorage.setItem("Sno",data.Sno);
+				location.href='ValidateRegister.html';
+			}else{
+				$("#emsg").show();
+				$("#emsg").html(data.Status);
+			}
+		}
+	});
+});
+
+$("#validate").on('submit', function(e){
+	e.preventDefault();
+	var fdata = $("#validate").serialize();
+	$.ajax({
+		url:server+"app_Validate.php",
+		data:fdata,
+		type:"post",
+		beforeSend: function(){
+			$("#loader").show();
+		},
+		success: function(data){
+			$("#loader").hide();
+			$("#emsg").hide();
+			if(data.Status=="Success"){
+				console.log(data);
+				localStorage.setItem("Sno",data.Sno);
+				localStorage.setItem("Phone",data.Phone);
+				location.href='index.html';
+			}else{
+				$("#emsg").show();
+				$("#emsg").html(data.Status);
+			}
+		}
+	});
+});
+
+$("#validateregister").on('submit', function(e){
+	e.preventDefault();
+	var fdata = $("#validateregister").serialize();
+	$.ajax({
+		url:server+"app_ValidateRegister.php",
+		data:fdata,
+		type:"post",
+		beforeSend: function(){
+			$("#loader").show();
+		},
+		success: function(data){
+			$("#loader").hide();
+			$("#emsg").hide();
+			if(data.Status=="Success"){
+				console.log(data);
+				localStorage.setItem("Sno",data.Sno);
+				localStorage.setItem("Phone",data.Phone);
+				location.href='index.html';
+			}else{
+				$("#emsg").show();
+				$("#emsg").html(data.Status);
+			}
+		}
+	});
+});
 });
 function connect(){
 	$.ajax({
@@ -52,3 +162,7 @@ function searchString(str){
 		$("#searchBox").hide();
 	}
 }
+
+function showBar(){
+	$("#lbar").toggle("slide", {direction: "left" }, 500);
+  }
